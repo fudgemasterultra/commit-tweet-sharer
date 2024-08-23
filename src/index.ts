1e941577b764ab0ccc5e23573b45b7c94101689c
fudgemasterultra <videogamecreative@gmail.com>
2024-08-23 10:08:45 -0400

    adding commit checking

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,13 @@ export default (app: Probot) => {
     });
     await context.octokit.issues.createComment(issueComment);
   });
+  app.on("commit_comment", async (context) => { 
+    //I want to grab the commit 
+    const commit = context.payload.comment.commit_id;
+    //I want to grab the repo
+    const repo = context.payload.repository.name;
+    //I want to grab the comment
+    const comment = context.payload.comment.body;
+    console.log(commit, repo, comment);
+  })
 };
